6fc3d3ee5780634db65e33b3a201e068e470ce9d
Rhiju Das <rhiju@stanford.edu>
2021-01-21 10:11:11 -0800

    03_SimpleNeuralNetworkTests init

---
+++ runs/03_SimpleNeuralNetworkTests.m
@@ -0,0 +1,4 @@
+% Let's get a bunch of training data to do the most basic test --
+% predict energy of a single conformation+pairing.
+
+d = load( '02_GenerateTrainingData.mat','all_x','all_p','all_sequence_onehot');
